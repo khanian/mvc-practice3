73563b71c9a6c7fa8cb9602e2266f72f53801ab4
khanian <khanylee@gmail.com>
2023-07-13 06:38:48 +0900

    first commit

--- src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -2,6 +2,8 @@ package org.example.mvc;
 
 import org.example.mvc.cotroller.Controller;
 import org.example.mvc.cotroller.HomeController;
+import org.example.mvc.cotroller.UserCreateController;
+import org.example.mvc.cotroller.UserListController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +13,8 @@ public class RequestMappingHandlerMapping {
 
     void init(){
         mappings.put("/", new HomeController());
+        mappings.put("/users", new UserListController());
+//        mappings.put("/users", new UserCreateController());
     }
 
     public Controller findHandler(String urlPath) {
---
+++ src/main/java/org/example/mvc/cotroller/UserListController.java
@@ -0,0 +1,13 @@
+package org.example.mvc.cotroller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class UserListController implements Controller{
+    @Override
+    public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.setAttribute("Users", List.of());
+        return "/user/list.jsp";
+    }
+}
