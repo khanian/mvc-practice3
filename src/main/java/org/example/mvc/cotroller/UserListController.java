2a6d00709ad2e223fdc9c641528c26c7d29eb9a0
khanian <khanylee@gmail.com>
2023-07-14 06:45:25 +0900

    add ViewResolver delete .jsp

--- src/main/java/org/example/mvc/cotroller/UserListController.java
+++ src/main/java/org/example/mvc/cotroller/UserListController.java
@@ -1,13 +1,14 @@
 package org.example.mvc.cotroller;
 
+import org.example.mvc.Repository.UserRepository;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class UserListController implements Controller{
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        request.setAttribute("Users", List.of());
+        request.setAttribute("Users", UserRepository.findAll());
         return "/user/list";
     }
 }
